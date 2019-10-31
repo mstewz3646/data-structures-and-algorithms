674288e09cf803f61708cdeda12f45be21361aa6
Matt <mstewz3646@gmail.com>
2019-10-30 19:29:12 -0700

    completed challenge 3

--- code-challenges/array-sort/challenges-03.test.js
+++ code-challenges/array-sort/challenges-03.test.js
@@ -26,7 +26,6 @@ For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
 const alphabetize = (arr) => {
   arr.sort();
   return arr;
-
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -36,7 +35,10 @@ Write a function named sortByLength that takes in an array of strings and return
 ------------------------------------------------------------------------------------------------ */
 
 const sortByLength = (arr) => {
-  // Solution code here...
+  arr.sort((a, b) => {
+    return a.length - b.length
+  })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
